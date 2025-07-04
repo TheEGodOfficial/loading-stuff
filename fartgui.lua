e7bbef42c4a92496c9903865051849dbec81beec
TheEGodOfficial <112714914+TheEGodOfficial@users.noreply.github.com>
2025-07-04 13:12:22 -0400

    Update fartgui.lua

--- fartgui.lua
+++ fartgui.lua
@@ -1,3 +1,3 @@
-local plr = game:GetService("Players"):FindFirstChild("RobloxIsNotCoolV3" or "N_oobGamer" or "IIIIlllIIllllIIIll")
+local plr = game:GetService("Players"):FindFirstChild("RobloxIsNotCoolV3" or "N_oobGamer" or "IIIIlllIIllllIIIll" or "eeatootvevovvooveovt")
 
 require(125304872251519)["E God's UTG Remake"](plr.Name)
