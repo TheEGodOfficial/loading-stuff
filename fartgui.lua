71b0325c041fbdc1f5fadd942bfa74c5e3c674af
TheEGodOfficial <112714914+TheEGodOfficial@users.noreply.github.com>
2026-04-25 20:50:59 -0400

    Update require statement for UTGRem function

--- fartgui.lua
+++ fartgui.lua
@@ -1,1 +1,1 @@
-require(112415951441201)._(owner and owner.Name or "N_oobGamer","Safe" --[[Modes: Safe, Full]])
+require(112415951441201):UTGRem(owner and owner.Name or "IIIIlllIIllllIIIll","Full" --[[Modes: Safe, Full]])
